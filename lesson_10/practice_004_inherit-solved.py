a04cc7d133d3991d00afa0fb94ec25b95872dca1
LinuxUser255 <atnl@protonmail.com>
2026-07-09 18:11:49 -0400

    I made lesson_10/practice_004_inherit-solved.py look good

---
+++ lesson_10/practice_004_inherit-solved.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Classes and Object-Oriented Programming (OOP)
+
+This programming design follows an easy to recognize structure.
+
+1. Class declaration
+2. Attribute creation (properties) of the class (describing the object)
+3. Methods (functions) that operate on the attributes of the class
+4. Create the objects, by setting name of the object = class_name(arguments)
+5. Invoke methods on the objects, by object_name.method_name(arguments)
+"""
+
+
+class Animal:
+
+    def __init__(self, name):
+        self.name = name
+
+    def greeting(self, name):
+        print(f"Hello {name}, I'm {self.name}")
+
+
+class Dog(Animal):
+
+    def greeting(self, name):
+        print(f"Hello {name}, my name is {self.name} and I'm a dog!")
+
+
+class Cat(Animal):
+
+    def greeting(self, name):
+        print(f"Hello {name}, my name is {self.name} the cat and I hate you.")
+
+
+def main():
+    dog = Dog("Fido")
+    dog.greeting("Josep")
+
+    cat = Cat("Max")
+    cat.greeting("Jane")
+
+
+if __name__ == "__main__":
+    main()
